9278728a2545a1c64bc4a82e76e22a8a23b712c4
Frizlab <fload@me.com>
2021-05-30 18:22:50 +0200

    Enhance NSConditionLock crash test

--- Tests/SignalHandlingTests/LockTest.swift
+++ Tests/SignalHandlingTests/LockTest.swift
@@ -6,13 +6,16 @@ import XCTest
 final class NSConditionLockTest : XCTestCase {
 	
 	func testNSConditionLock() throws {
+		/* https://bugs.swift.org/browse/SR-14676 */
 //		Thread(block: { NSConditionLock(condition: 0).lock(whenCondition: 1) }).start()
 		Thread.sleep(forTimeInterval: 0.1)
 		
 		#if !os(Linux)
 		XCTAssert(true)
 		#else
-		XCTAssert(false, "This test crashes on Linux for now with Swift 5.4.1 (when the NSConditionLock line is uncommented); when it does not crash there will be something to do in the SwiftHandling code. So we fail the test to remember that.")
+		/* Apparently XCTExpectFailure does not exist on Linux. */
+//		XCTExpectFailure("Linux has a crash in NSConditionLock. This test is only here to remember to check if the bug is fixed from time to time (simply uncomment the second line of the test; if test does not crash on Linux we’re good).")
+		XCTAssert(false, "Linux has a crash in NSConditionLock. This test is only here to remember to check if the bug is fixed from time to time (simply uncomment the second line of the test; if test does not crash on Linux we’re good).")
 		#endif
 	}
 	
