c65f0660ddedfdb8bb2cedae763d23e9ba97b381
Frizlab <fload@me.com>
2025-01-02 09:09:02 +0100

    Make the dependency on swift-system explicit
    Optional dependencies do not exist in SPM and the solution I implemented was not deterministic (the SystemPackage availability if in the package graph was depending on the order of compilation of the packages, which is more or less random).

--- Package@swift-5.8.swift
+++ Package@swift-5.8.swift
@@ -18,11 +18,16 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-log.git",             from: "1.4.2"),
+		/* We’re aware of the existence of System on macOS. After some thinking/research, we decided to agree with <https://forums.swift.org/t/50719/5>.
+		 * Basically what we want is optional dependencies, but they are not implemented (nor planned) in SPM for now <https://forums.swift.org/t/swiftpm-canimport/11749>.
+		 * There are also cross-import overlays that exist (<https://sundayswift.com/posts/cross-import-overlays/>), but they are not possible to do with SPM (and are not what I really want here anyway). */
+		.package(url: "https://github.com/apple/swift-system.git",          from: "1.0.0"),
 		.package(url: "https://github.com/xcode-actions/clt-logger.git",    from: "1.0.0-beta.4"),
 	],
 	targets: [
 		.target(name: "SignalHandling", dependencies: [
-			.product(name: "Logging", package: "swift-log"),
+			.product(name: "Logging",       package: "swift-log"),
+			.product(name: "SystemPackage", package: "swift-system"),
 		], swiftSettings: swiftSettings),
 		
 		.executableTarget(name: "signal-handling-tests-helper", dependencies: [
@@ -33,8 +38,8 @@ let package = Package(
 		], swiftSettings: swiftSettings),
 		.testTarget(name: "SignalHandlingTests", dependencies: [
 			.target(name: "signal-handling-tests-helper"),
-			.product(name: "CLTLogger",     package: "clt-logger"),
-			.product(name: "Logging",       package: "swift-log"),
+			.product(name: "CLTLogger", package: "clt-logger"),
+			.product(name: "Logging",   package: "swift-log"),
 		], swiftSettings: swiftSettings),
 	]
 )
